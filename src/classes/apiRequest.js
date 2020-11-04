812cafd4e0e01df4cce42079f6d4402b4a7d31e8
Scott Brabson <scottbrabson@gmail.com>
2020-11-04 15:26:45 -0700

    Add error console log to fetch methods

--- src/classes/apiRequest.js
+++ src/classes/apiRequest.js
@@ -3,21 +3,21 @@ const apiRequest = {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
       .then(response => response.json())
       .then(data => data.userData)
-      .catch(error => console.log(error));
+      .catch(error => console.log(error, "Encountered an error"));
   },
 
   getRoomData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
       .then(response => response.json())
       .then(data => data.roomData)
-      .catch(error => console.log(error));
+      .catch(error => console.log(error, "Encountered an error"));
   },
 
   getBookingData() {
     return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
       .then(response => response.json())
       .then(data => data.bookingData)
-      .catch(error => console.log(error));
+      .catch(error => console.log(error, "Encountered an error"));
   },
 
   createBooking(booking) {
@@ -29,19 +29,21 @@ const apiRequest = {
       body: JSON.stringify(booking),
     })
       .then(response => response.json())
-      .catch(error => console.log(error))
+      .then(response => console.log("Booking successfully CREATED"))
+      .catch(error => console.log(error, "Encountered an error"))
   },
 
-  deleteBooking(id) {
+  deleteBooking(booking) {
     fetch('https://fe-apps.herokuapp.com/api/v1/fitlit/1908/sleep/sleepData', {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(),
+      body: JSON.stringify(booking),
     })
       .then(response => response.json())
-      .catch(error => console.log(error))
+      .then(response => console.log("Booking successfully DELETED"))
+      .catch(error => console.log(error, "Encountered an error"))
   }
 };
 

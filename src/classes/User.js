245046bc4fbfbb0bbbfa1e34e1cc26ad6f7e0eb0
Scott Brabson <scottbrabson@gmail.com>
2020-11-05 14:31:41 -0700

    Add future TODO in comments

--- src/classes/User.js
+++ src/classes/User.js
@@ -41,7 +41,7 @@ export default class User {
   }
   viewAvailableRooms(bookingData, roomData, date) {
     let unavailableRooms = this.viewUnavailableRooms(bookingData, roomData, date)
-    return roomData.filter(room => !unavailableRooms.includes(room))
+    return roomData.filter(room => !unavailableRooms.includes(room)) // not empathetic consider rewriting
   }
   viewAvailableRoomsByType(bookingData, roomData, date, roomType) {
     let availableRooms = this.viewAvailableRooms(bookingData, roomData, date);

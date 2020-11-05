3da5d4e2e6d02d574e1a04e658bab010f0be315f
Scott Brabson <scottbrabson@gmail.com>
2020-11-05 12:37:09 -0700

    Begin main script buildout - fetched data

--- src/index.js
+++ src/index.js
@@ -8,3 +8,32 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+let userData;
+let bookingData;
+let roomData;
+let user = new User()
+
+import User from './classes/User';
+import Manager from './classes/Manager';
+import Booking from './classes/Booking';
+
+import { domObject } from './classes/domObject';
+import { apiRequest } from './classes/apiRequest';
+
+const fetchedUserData = apiRequest.getUserData();
+const fetchedBookingData = apiRequest.getBookingData();
+const fetchedRoomData = apiRequest.getRoomData();
+
+Promise.all([fetchedUserData, fetchedBookingData, fetchedRoomData])
+  .then(value => {
+    userData = value[0];
+    bookingData = value[1];
+    roomData = value[2];
+    loadApp();
+  })
+  .catch(error => console.log(error))
+
+function loadApp() {
+  console.log(userData);
+}

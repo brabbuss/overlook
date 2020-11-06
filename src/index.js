39ada002a93b421bd7032a8831df106c42a86204
Scott Brabson <scottbrabson@gmail.com>
2020-11-05 22:27:40 -0700

    Begin HTML/Styling buildout

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import './images/hotel-exterior.jpg'
+import './images/room-kinfolk.jpg'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 let userData;

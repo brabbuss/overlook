62ed7b06842d2527b18b8f9cb03a828ae8c24c6c
Scott Brabson <scottbrabson@gmail.com>
2020-11-07 18:46:49 -0700

    Add assets

---
+++ src/images/image-assets.js
@@ -0,0 +1,22 @@
+export const roomImages = [
+  './images/room-gaudy.jpg',
+  './images/room-kingly.jpg',
+  './images/room-kinfolk.jpg',
+  './images/room-modish.jpg',
+  './images/room-small.jpg',
+  './images/room-sparse.jpg',
+  './images/room-suite.jpg',
+  './images/room-xtrasmall.jpg',
+  './images/room-xtrasmall2.jpg',
+]
+
+export const bathroomImages = [
+  './images/room-bath-fancy.jpg',
+  './images/room-bath-fancy2.jpg',
+]
+
+export const lobbyImages = [
+  './images/hotel-lobby-mod1.jpg',
+  './images/hotel-lobby-mod2.jpg',
+  './images/hotel-lobby-mod3.jpg'
+]
